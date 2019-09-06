092a52ee8df797dc193a02f3e87676e09a0d2bae
KejariwalAyush <kejayush29@gmail.com>
2019-09-06 15:04:50 +0530

    images added to chessboard2

--- chessboard/chessboard2.java
+++ chessboard/chessboard2.java
@@ -3,8 +3,27 @@ import javax.swing.*;
   
 public class chessboard2{  
 JFrame f;  
+
+    ImageIcon ibking = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black king.png");
+    ImageIcon ibqueen = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black queen.png");
+    ImageIcon ibcamel = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black camel.png");
+    ImageIcon ibhorse = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black horse.png");
+    ImageIcon ibelephant = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black elephant.png");
+    ImageIcon ibsoilder = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black soilder.png");  
+    
+    ImageIcon iwking = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\white king.png");
+    ImageIcon iwqueen = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\white queen.png");
+    ImageIcon iwcamel = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\white camel.png");
+    ImageIcon iwhorse = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\white horse.png");
+    ImageIcon iwelephant = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\white elephant.png");
+    ImageIcon iwsoilder = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\white soilder.png");  
+    
+
+
 chessboard2(){  
-    f=new JFrame();  
+    f=new JFrame(); 
+
+    //b[row][column] 
       
     JButton b[][]=new JButton[8][8];  
              
@@ -28,9 +47,9 @@ chessboard2(){
     }  
   
     f.setLayout(new GridLayout(8,8));  
-    //setting grid layout of 3 rows and 3 columns  
+    //setting grid layout of 8 rows and 8 columns  
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.setSize(600,600);  
+    f.setSize(700,700);  
     f.setVisible(true); 
 
     b[0][0].setText("b rook");
@@ -54,10 +73,29 @@ chessboard2(){
     for(int k=0;k<8;k++){
         b[1][k].setText("b Pawn");
         b[6][k].setText("w Pawn");
+        b[1][k].setIcon(ibsoilder);
+        b[6][k].setIcon(iwsoilder);
     }
 
-
-   // f.rotate(); 
+    //b[row][column] 
+   
+    b[7][4].setIcon(iwking);
+    b[7][3].setIcon(iwqueen);
+    b[7][2].setIcon(iwcamel);
+    b[7][1].setIcon(iwhorse);
+    b[7][0].setIcon(iwelephant);
+    b[7][5].setIcon(iwcamel);
+    b[7][6].setIcon(iwhorse);
+    b[7][7].setIcon(iwelephant);
+    
+    b[0][3].setIcon(ibking);
+    b[0][4].setIcon(ibqueen);
+    b[0][2].setIcon(ibcamel);
+    b[0][1].setIcon(ibhorse);
+    b[0][0].setIcon(ibelephant);
+    b[0][5].setIcon(ibcamel);
+    b[0][6].setIcon(ibhorse);
+    b[0][7].setIcon(ibelephant);
 }  
 
 public static void main(String[] args) {  
