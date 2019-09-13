80ad7c20cb9c70f994e79bb84f5d58c67e39540c
KejariwalAyush <kejayush29@gmail.com>
2019-09-13 19:19:19 +0530

    objects started moving and when king dies game ends

--- chessboard/chessboard2.java
+++ chessboard/chessboard2.java
@@ -1,8 +1,16 @@
 import java.awt.*;  
-import javax.swing.*;  
+import javax.swing.*;
+import java.awt.event.*;  
   
-public class chessboard2{  
-JFrame f;  
+ class chessboard2 implements ActionListener //, MouseMotionListener
+ {  
+JFrame f; 
+ //b[row][column] 
+JButton b[][]=new JButton[8][8];  
+JButton src = new JButton();
+JButton temp = new JButton();
+JButton src2 = new JButton();
+JButton blank = new JButton();
 
     ImageIcon ibking = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black king.png");
     ImageIcon ibqueen = new ImageIcon("C:\\Users\\Kejariwal\\Desktop\\chess pieses\\black queen.png");
@@ -21,11 +29,12 @@ JFrame f;
 
 
 chessboard2(){  
+//f.addMouseListener();
     f=new JFrame(); 
 
     //b[row][column] 
       
-    JButton b[][]=new JButton[8][8];  
+     
              
     for(int i=0;i<8;i++){
         for(int j=0;j<8;j++){
@@ -39,7 +48,7 @@ chessboard2(){
             else
                 b[i][j].setBackground(Color.white);
             
-
+            b[i][j].addActionListener(this);
 
         }
 
@@ -52,32 +61,27 @@ chessboard2(){
     f.setSize(700,700);  
     f.setVisible(true); 
 
-    b[0][0].setText("b rook");
-    b[0][7].setText("b rook");
-    b[0][1].setText("b knight");
-    b[0][6].setText("b knight");
-    b[0][2].setText("b bishop");
-    b[0][5].setText("b bishop");
-    b[0][3].setText("b king");
-    b[0][4].setText("b queen");
-
-    b[7][0].setText("w rook");
-    b[7][7].setText("w rook");
-    b[7][1].setText("w knight");
-    b[7][6].setText("w knight");
-    b[7][2].setText("w bishop");
-    b[7][5].setText("w bishop");
-    b[7][4].setText("w king");
-    b[7][3].setText("w queen");
-
     for(int k=0;k<8;k++){
-        b[1][k].setText("b Pawn");
-        b[6][k].setText("w Pawn");
+    	b[2][k].setText("");
+    	b[3][k].setText("");
+    	b[4][k].setText("");
+    	b[5][k].setText("");
+
+    	b[1][k].setText("b");
+    	b[0][k].setText("b");
+    	b[6][k].setText("w");
+    	b[7][k].setText("w");
+
         b[1][k].setIcon(ibsoilder);
         b[6][k].setIcon(iwsoilder);
     }
 
     //b[row][column] 
+    b[0][3].setText("bk");
+    b[0][4].setText("bq");
+    b[7][3].setText("wq");
+    b[7][4].setText("wk");
+
    
     b[7][4].setIcon(iwking);
     b[7][3].setIcon(iwqueen);
@@ -101,4 +105,77 @@ chessboard2(){
 public static void main(String[] args) {  
     new chessboard2();  
 }  
+
+int x,y,cnt=0;
+int input=2;
+
+public void actionPerformed(ActionEvent e)
+{
+	if(e.getSource() instanceof JButton)
+	{
+		src = (JButton) e.getSource();
+
+	}
+	if(cnt==0 && src.getText()!="")
+	{
+		temp.setIcon(src.getIcon());
+		temp.setText(src.getText());
+		src.setIcon(null);
+		src.setText("");
+		cnt++;
+	}
+	else
+	{
+		src2.setIcon(src.getIcon());
+		src2.setText(src.getText());
+		if(src2.getText()!=temp.getText())
+		{
+			src.setIcon(temp.getIcon());
+			src.setText(temp.getText());
+		}
+		temp.setIcon(null);
+		temp.setText("");
+		cnt=0;
+	}
+
+
+	if(src2.getText()=="wk"||src2.getText()=="bk")
+	{
+
+			input = JOptionPane.showConfirmDialog(f, (src2.getText().charAt(0))+" want to restart?");
+			if(input == 1)System.exit(0);
+			else if(input == 0)restart();
+			
+	}
+	//if(src.getIcon().equals(ibsoilder)){
+	//	b[x+1][y].setIcon(ibsoilder);
+	//	src.setIcon(null);
+	//}
+}
+
+	// getX() and getY() functions return the 
+    // x and y coordinates of the current 
+    // mouse position 
+    // getClickCount() returns the number of 
+    // quick consecutive clicks made by the user 
+
+/*public void mousePressed(MouseEvent e){
+	
+	int x=e.getX();
+	int y=e.getY();
+	b[x][y].setBackground(Color.yellow);
+
+	for(int a=0;a<8;a++){
+	if(b[x][y]==b[1][a])
+		b[2][a].setBackground(Color.pink);
+
+	}
+	//b[1][1].setText("hey");
+	}*/
+
+	public void restart()
+	{
+		new chessboard2();
+	}
 }  
+
